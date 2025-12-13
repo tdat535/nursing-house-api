0d4d3cc415464ec2178480bc902faef0897943e3
tdat <datnguyen4804@gmail.com>
2025-12-13 09:21:02 +0700

    final3

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -13,13 +13,24 @@ export class UsersService {
 
     @InjectRepository(Role)
     private rolesRepo: Repository<Role>,
-  ) {}
+  ) { }
 
-  async createUser(email: string, password: string, role: number) {
-    const hashed = await bcrypt.hash(password, 10);
+  async createUser(email: string, password: string, roleId: number) {
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    // Tìm role entity
+    const role = await this.rolesRepo.findOne({ where: { id: roleId } });
+    if (!role) {
+      throw new BadRequestException('Role không tồn tại');
+    }
 
-    const user = this.usersRepo.create({ email, password: hashed });
+    const user = this.usersRepo.create({
+      email,
+      password: hashedPassword,
+      role, // gắn entity Role
+    });
+
     return this.usersRepo.save(user);
   }
 
